08d556e7ef75c9a837e69688b22860b53dfa9ff1
Danillo Fidel <danillopkt@hotmail.com>
2018-07-29 19:00:10 -0300

    components

--- projects/app1/src/app/app.module.ts
+++ projects/app1/src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { HeaderComponent } from "./header/header.component";
+import { PanelComponent } from "./panel/panel.component";
+import { AttemptsComponent } from './attempts/attempts.component';
+import { ProgressComponent } from './progress/progress.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, HeaderComponent, PanelComponent, AttemptsComponent, ProgressComponent],
+  imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ projects/app1/src/app/header/header.component.ts
@@ -0,0 +1,12 @@
+//Fazer importação do decorador Component
+import { Component, OnInit } from "@angular/core";
+@Component({
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.css"]
+})
+export class HeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
